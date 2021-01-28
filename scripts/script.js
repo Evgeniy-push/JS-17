85f888914522b6e3ae349a5df6d0ad881aedf94a
Evgeniy Pushkarev <joxan_p@mail.ru>
2021-01-29 01:29:32 +0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+    let money,
+        income,
+        addExpenses,
+        deposit,
+        mission,
+        period;
+
+alert('hello alert');
+console.log('hello console');
+
+});
