bca21e197d9c31ab1915c2b1e6c9eae12a362db0
Evgeniy Pushkarev <joxan_p@mail.ru>
2021-01-30 01:26:49 +0700

    lesson-02

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-    let money,
-        income,
-        addExpenses,
-        deposit,
-        mission,
-        period;
+    let money = 30000,
+        income = 'распил',
+        addExpenses = 'Интернет, Мобильный, Бензин' ,
+        deposit = true,
+        mission = 500000,
+        period = 12;
 
-alert('hello alert');
-console.log('hello console');
-
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log(addExpenses.length);
+    console.log(`Период равен ${period} месяцев`);
+    addExpenses = addExpenses.toLowerCase().split(', ');
+    console.log('массив addExpenses: ', addExpenses);
+    let budgetDay = money / 30;
+    console.log('budgetDay: ', budgetDay);
 });
